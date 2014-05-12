0b71cd7c2a3058769281d269400c6f44c6f14dcb
Josh Gough <jsgough@gmail.com>
2014-05-12 08:35:19 -0400

    plot2, plot3

--- plot2.R
+++ plot2.R
@@ -2,6 +2,7 @@ data <- read.csv(file="household_power_consumption.txt", sep=";", header=T, colC
 data$Datetime <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 subd <- subset(data, Datetime >= as.POSIXlt("2007-02-01 00:00:00") & Datetime < as.POSIXlt("2007-02-03 00:00:00"))
 # OK: returns expected 2880 rows
-hist(subd$Global_active_power, col='red', main='Global Active Power', xlab='Global Active Power (kilowatts)')
-dev.copy(png, file='plot1.png')
+plot(x=subd$Datetime, y=subd$Global_active_power, ylab="Global Active Power (kilowatts)", xlab="", type='n')
+lines(x=subd$Datetime, y=subd$Global_active_power)
+dev.copy(png, file='plot2.png')
 dev.off()
--- plot3.R
+++ plot3.R
@@ -2,6 +2,9 @@ data <- read.csv(file="household_power_consumption.txt", sep=";", header=T, colC
 data$Datetime <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 subd <- subset(data, Datetime >= as.POSIXlt("2007-02-01 00:00:00") & Datetime < as.POSIXlt("2007-02-03 00:00:00"))
 # OK: returns expected 2880 rows
-hist(subd$Global_active_power, col='red', main='Global Active Power', xlab='Global Active Power (kilowatts)')
-dev.copy(png, file='plot1.png')
+plot(x=subd$Datetime, y=subd$Sub_metering_1, ylab="Energy sub metering", xlab="", type='n')
+lines(x=subd$Datetime, y=subd$Sub_metering_1)
+lines(x=subd$Datetime, y=subd$Sub_metering_2, col="red")
+lines(x=subd$Datetime, y=subd$Sub_metering_3, col="blue")
+dev.copy(png, file='plot3.png')
 dev.off()
