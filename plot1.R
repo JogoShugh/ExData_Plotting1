851c7ff3b8fc86c61cf41cbeb2cb2edf9c496196
Josh Gough <jsgough@gmail.com>
2014-05-12 13:05:01 -0400

    Using png not dev.copy for images

--- plot1.R
+++ plot1.R
@@ -2,6 +2,9 @@ data <- read.csv(file="household_power_consumption.txt", sep=";", header=T, colC
 data$Datetime <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 subd <- subset(data, Datetime >= as.POSIXlt("2007-02-01 00:00:00") & Datetime < as.POSIXlt("2007-02-03 00:00:00"))
 # OK: returns expected 2880 rows
+
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
 hist(subd$Global_active_power, col='red', main='Global Active Power', xlab='Global Active Power (kilowatts)')
-dev.copy(png, file='plot1.png')
+
 dev.off()
--- plot2.R
+++ plot2.R
@@ -2,7 +2,9 @@ data <- read.csv(file="household_power_consumption.txt", sep=";", header=T, colC
 data$Datetime <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 subd <- subset(data, Datetime >= as.POSIXlt("2007-02-01 00:00:00") & Datetime < as.POSIXlt("2007-02-03 00:00:00"))
 # OK: returns expected 2880 rows
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+
 plot(x=subd$Datetime, y=subd$Global_active_power, ylab="Global Active Power (kilowatts)", xlab="", type='n')
 lines(x=subd$Datetime, y=subd$Global_active_power)
-dev.copy(png, file='plot2.png')
+
 dev.off()
